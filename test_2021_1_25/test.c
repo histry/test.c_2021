0c0cbcbb4422ec045759ac31256bce335ed9d6ec
histry <2630314901@qq.com>
2021-01-24 16:01:18 +0800

    C语言100道经典编程

---
+++ test_2021_1_25/test.c
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+	int ret = 0;
+	char password[20] = { 0 };
+
+
+	return 0;
+}
+//int main()
+//{
+//	int ch = 0;
+//	//end of file----EOF文件结束标志
+//	while ((ch = getchar()) != EOF)
+//	{
+//		putchar(ch);
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int ch = getchar();//接受getchar的返回值
+//	putchar(ch);
+//	return 0;
+//}
+//int main()
+//{
+//	int i = 0;
+//	for (i = 0; i < 5; i++)
+//	{
+//		int ch = getchar();
+//		getchar();
+//		putchar(ch);
+//	}
+//	return 0;
+//}
