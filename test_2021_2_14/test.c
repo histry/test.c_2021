d454d91dedb0f9fcd24fbfd62f5a1413a6f8803e
histry <2630314901@qq.com>
2021-02-14 23:10:34 +0800

    代码上传

---
+++ test_2021_2_14/test.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	short a = 10;
+	short int b = 10;
+	signed short int c = 10;
+
+	int d = 10;
+	signed int e = 10;
+
+	return 0;
+}
+//#include<stdio.h>
+//int main()
+//{
+//	int i = 0;
+//	for(i = 1; i <= 100; i++)
+//	{
+//		printf("%-2d ", i);
+//		if (i % 10 == 0)
+//			printf("\n");
+//	}
+//	return 0;
+//}
